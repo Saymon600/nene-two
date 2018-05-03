49545610aaa340e5f18e0fff14cf9d8fac2b8c33
Takahisa <samuel.y.yamaguchi@gmail.com>
2018-05-03 00:16:05 -0300

    nene config ignore watch

--- nene.config.js
+++ nene.config.js
@@ -3,6 +3,7 @@ module.exports = {
       name        : "nene-two",
       script      : "./server.js",
       watch       : true,
+      ignore_watch: ["node_modules","music"],
       env: {
         "NODE_ENV": "development",
         "TOKEN": "",
